4223497f150449fd0d9afd8211e2ecc34d8d85b3
Mark <markklepanchuk@gmail.com>
2026-09-08 14:49:45 +0200

    feat: compare task

---
+++ task1/compare.c
@@ -0,0 +1,15 @@
+int compare(const int a, const int b)
+{
+    if (a < b)
+    {
+        return -1;
+    }
+    else if (a > b)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
--- task1/main.c
+++ task1/main.c
@@ -1,15 +1,26 @@
+#include "compare.h"
 #include <stdio.h>
-
 #include "factorial.h"
 
 int main()
 {
     int n;
 
+    printf("This program calculates the factorial of a number.\n");
     printf("Enter a number: ");
     scanf("%d", &n);
 
     printf("%d! = %llu\n", n, factorial(n));
+
+    printf("Now let's compare two numbers.\n");
+    int a, b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    int result = compare(a, b);
+    printf("Comparison result: %d\n", result);
 
     return 0;
 }
