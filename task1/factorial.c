96d0c176e7f32e37508ebbfadeb48e1f680fcf8b
Mark <markklepanchuk@gmail.com>
2026-09-08 22:03:59 +0200

    feat: enhance factorial calculation and add input validation

--- task1/factorial.c
+++ task1/factorial.c
@@ -1,5 +1,10 @@
 unsigned long long factorial(unsigned int n)
 {
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
     for (unsigned int i = n - 1; i > 0; i--)
     {
         n *= i;
--- task1/main.c
+++ task1/main.c
@@ -1,26 +1,79 @@
 #include "compare.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "factorial.h"
+
+// correctness design style
+
+void show_menu(void);
 
 int main()
 {
-    int n;
+    int running = 1;
+    while (running)
+    {
+        show_menu();
 
-    printf("This program calculates the factorial of a number.\n");
-    printf("Enter a number: ");
-    scanf("%d", &n);
+        char choice;
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
 
-    printf("%d! = %llu\n", n, factorial(n));
+        switch (choice)
+        {
+        case '1':
+        {
+            int n;
+            printf("Enter a non-negative integer: ");
+            if (scanf("%d", &n) != 1)
+            {
+                printf("Error: Invalid input. Please enter a valid integer.\n");
 
-    printf("Now let's compare two numbers.\n");
-    int a, b;
-    printf("Enter the first number: ");
-    scanf("%d", &a);
-    printf("Enter the second number: ");
-    scanf("%d", &b);
+                while (getchar() != '\n')
+                {
+                }
+            }
+            else if (n < 0)
+            {
+                printf("Error: Factorial is not defined for negative numbers.\n");
+            }
+            else
+            {
+                unsigned long long result = factorial(n);
+                printf("Factorial of %d is %llu\n", n, result);
+            }
 
-    int result = compare(a, b);
-    printf("Comparison result: %d\n", result);
+            break;
+        }
+        case '2':
+        {
+            int a, b;
+            printf("Enter two integers: ");
+            scanf("%d %d", &a, &b);
+            int cmp_result = compare(a, b);
+            printf("Comparison result: %d\n", cmp_result);
+
+            break;
+        }
+        case '3':
+        {
+            printf("Exiting the program.\n");
+            running = 0;
+
+            break;
+        }
+        default:
+        {
+            printf("Invalid choice. Please try again.\n");
+        }
+        }
+    }
 
     return 0;
+}
+
+void show_menu(void)
+{
+    printf("1. Calculate Factorial\n");
+    printf("2. Compare Two Numbers\n");
+    printf("3. Exit\n");
 }
